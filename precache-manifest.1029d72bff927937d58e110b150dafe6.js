5dcbc333fa30462d0a1193302c5af565cfd0f150
Santiago Hernández <santiagohernandez.1997@gmail.com>
2019-08-02 12:22:19 -0300

    Updates

---
+++ precache-manifest.1029d72bff927937d58e110b150dafe6.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1b09fa1a4b5c905a0f8990ddc5fcb1d",
+    "url": "/share-event-demo/index.html"
+  },
+  {
+    "revision": "afdb43d42359178c4bac",
+    "url": "/share-event-demo/static/css/main.64002cdf.chunk.css"
+  },
+  {
+    "revision": "f7372c95649fdf8ad37d",
+    "url": "/share-event-demo/static/js/2.13179017.chunk.js"
+  },
+  {
+    "revision": "afdb43d42359178c4bac",
+    "url": "/share-event-demo/static/js/main.1366605b.chunk.js"
+  },
+  {
+    "revision": "0408b5f41db0ddbbaba4",
+    "url": "/share-event-demo/static/js/runtime~main.bd33bcab.js"
+  },
+  {
+    "revision": "7f36828db317ce480e74981ab316705a",
+    "url": "/share-event-demo/static/media/apple.7f36828d.svg"
+  },
+  {
+    "revision": "686f8efa6e3e28e96d1c08399e8d353d",
+    "url": "/share-event-demo/static/media/google.686f8efa.svg"
+  },
+  {
+    "revision": "a1293d2c85dd3ef4a5555912620b449c",
+    "url": "/share-event-demo/static/media/outlook.a1293d2c.svg"
+  },
+  {
+    "revision": "d7fcfd93f140407085c303861db2692e",
+    "url": "/share-event-demo/static/media/yahoo.d7fcfd93.svg"
+  }
+]);
